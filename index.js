4b83a675ce5d7b542f5858b3aa6c2c465435f403
chris <chris.mahfouz@hotmail.com>
2021-02-27 18:33:48 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var employee={
+    name:'chris',
+    streetAddress:'nahr'
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    var newObj={...obj};
+    newObj[key]=value;
+    return newObj
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+obj[key]=value;
+return obj
+}
+function deleteFromEmployeeByKey(obj,key){
+var newObj={...obj}
+delete newObj[key]
+return newObj
+}
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+delete obj[key]
+return obj
+}
